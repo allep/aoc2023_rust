5470e6834192b0212c4df3c8ae9edf7c74e3045e
Alessandro Paganelli <alessandro.paganelli@gmail.com>
2024-11-13 23:13:06 +0100

    Adds first possible approach

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,8 +1,44 @@
+struct AlmanacEntry {
+    output_start: i32,
+    input_start: i32,
+    input_range: i32,
+}
+
+fn mapping_logic_impl(x: &i32, map: &Vec<AlmanacEntry>) -> i32 {
+    for entry in map {
+        if *x >= entry.input_start && *x <= entry.input_start + entry.input_range {
+            return entry.output_start + *x - entry.input_start;
+        }
+    }
+    *x
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn proper_mapping() {
+        let input = vec![79, 14, 55, 13];
+
+        let seed_to_soil_almanac: Vec<AlmanacEntry> = vec![
+            AlmanacEntry {
+                output_start: 50,
+                input_start: 98,
+                input_range: 2,
+            },
+            AlmanacEntry {
+                output_start: 52,
+                input_start: 50,
+                input_range: 48,
+            },
+        ];
+
+        let seed_to_soil_mapping = |x: &i32| -> i32 {
+            mapping_logic_impl(x, &seed_to_soil_almanac)
+        };
+
+        let output: Vec<_> = input.iter().map(seed_to_soil_mapping).collect();
+        assert_eq!(output, vec![81, 14, 57, 13]);
     }
 }
